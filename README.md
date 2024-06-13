06900d253b2f68bd514bb30a7dabc7d496feb8fb
Guebbit <guerzoni.andrea91@gmail.com>
2024-06-14 01:01:00 +0200

    version 1.0.3

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 TODO
- - Install previous stable version of itself and add to the tests to double check
- - Documentation
- - Some tests were hastly made with chatgpt, redo
+ - Make Documentation
--- tests/whitelist.test.ts
+++ tests/whitelist.test.ts
@@ -1,24 +1,16 @@
 import { whitelist } from '../src';
 
 describe("(whitelist) Filter object by array of allowed values", () => {
-
   test("I filter out every key-value pair except 'id' and 'param1'", () => {
-    const inputObject = {
+    expect(whitelist({
       id: 3,
       param1: 'adipiscing',
       param2: 'elit sed do',
       param3: 'eiusmod'
-    };
-    const allowedKeys = ['id', 'param1'];
-
-    const expectedOutput = {
+    }, ['id', 'param1'])).toEqual({
       id: 3,
       param1: 'adipiscing',
-    };
-
-    // Test your implementation
-    expect(whitelist(inputObject, allowedKeys)).toEqual(expectedOutput);
+    });
   });
-
 });
 
