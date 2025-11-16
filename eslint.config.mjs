15735c58944f763258c2ca33703ce1a5297bdf04
Guebbit <guerzoni.andrea91@gmail.com>
2025-11-16 02:06:23 +0100

    version 1.0.7

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,186 +4,183 @@ import tseslint from 'typescript-eslint'
 import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
 export default tseslint.config(
-  {
-    ignores: [
-      'dist',
-      'node_modules',
-      'eslint.config.mjs',
-      'docs',
-    ]
-  },
-
-  /**
-   *
-   */
-  eslint.configs.recommended,
-
-  /**
-   *
-   */
-  tseslint.configs.recommendedTypeChecked,
-
-  /**
-   *
-   */
-  tseslint.configs.strictTypeChecked,
-
-  /**
-   *
-   */
-  tseslint.configs.stylisticTypeChecked,
-
-  /**
-   *
-   */
-  eslintPluginUnicorn.configs['flat/recommended'],
-
-  /**
-   *
-   */
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname
-      }
-    }
-  },
-
-  /**
-   *
-   */
-  {
-    plugins: {
-      // unicorn: eslintPluginUnicorn
+    {
+        ignores: ['dist', 'node_modules', 'eslint.config.mjs', 'docs']
     },
 
-    languageOptions: {
-      globals: {
-        ...globals.browser
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module'
+    /**
+     *
+     */
+    eslint.configs.recommended,
+
+    /**
+     *
+     */
+    tseslint.configs.recommendedTypeChecked,
+
+    /**
+     *
+     */
+    tseslint.configs.strictTypeChecked,
+
+    /**
+     *
+     */
+    tseslint.configs.stylisticTypeChecked,
+
+    /**
+     *
+     */
+    eslintPluginUnicorn.configs['flat/recommended'],
+
+    /**
+     *
+     */
+    {
+        languageOptions: {
+            parserOptions: {
+                // projectService: true, this would create problems with tests, better to use a specific tsconfig
+                project: ['./tsconfig.eslint.json'],
+                tsconfigRootDir: import.meta.dirname
+            }
+        }
     },
 
-    rules: {
-      'no-console': 'warn',
-      'no-debugger': 'warn',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      'no-nested-ternary': 'off',
+    /**
+     *
+     */
+    {
+        plugins: {
+            // unicorn: eslintPluginUnicorn
+        },
 
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          selector: 'default',
-          format: ['camelCase', 'PascalCase'],
-          leadingUnderscore: 'allow',
-          trailingUnderscore: 'allow'
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            },
+            ecmaVersion: 'latest',
+            sourceType: 'module'
         },
-        {
-          selector: 'variable',
-          format: ['camelCase', 'UPPER_CASE'],
-          leadingUnderscore: 'allow',
-          trailingUnderscore: 'allow'
-        },
-        {
-          selector: ['class', 'typeLike', 'typeParameter', 'enum'],
-          format: ['PascalCase']
-        },
-        {
-          selector: ['function'],
-          format: ['camelCase'],
-          leadingUnderscore: 'allow'
-        },
-        {
-          selector: 'interface',
-          format: ['PascalCase'],
 
-          custom: {
-            regex: '^I[A-Z]',
-            match: true
-          }
-        },
-        {
-          selector: 'enum',
-          format: ['PascalCase'],
+        rules: {
+            'no-console': 'warn',
+            'no-debugger': 'warn',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            'no-nested-ternary': 'off',
 
-          custom: {
-            regex: '^E[A-Z]',
-            match: true
-          }
-        },
-        {
-          selector: ['memberLike', 'enumMember'],
-          format: ['camelCase', 'PascalCase', 'UPPER_CASE', 'snake_case'],
-          leadingUnderscore: 'allow',
-          trailingUnderscore: 'allow'
+            '@typescript-eslint/naming-convention': [
+                'error',
+                {
+                    selector: 'default',
+                    format: ['camelCase', 'PascalCase'],
+                    leadingUnderscore: 'allow',
+                    trailingUnderscore: 'allow'
+                },
+                {
+                    selector: 'variable',
+                    format: ['camelCase', 'UPPER_CASE'],
+                    leadingUnderscore: 'allow',
+                    trailingUnderscore: 'allow'
+                },
+                {
+                    selector: ['class', 'typeLike', 'typeParameter', 'enum'],
+                    format: ['PascalCase']
+                },
+                {
+                    selector: ['function'],
+                    format: ['camelCase'],
+                    leadingUnderscore: 'allow'
+                },
+                {
+                    selector: 'interface',
+                    format: ['PascalCase'],
+
+                    custom: {
+                        regex: '^I[A-Z]',
+                        match: true
+                    }
+                },
+                {
+                    selector: 'enum',
+                    format: ['PascalCase'],
+
+                    custom: {
+                        regex: '^E[A-Z]',
+                        match: true
+                    }
+                },
+                {
+                    selector: ['memberLike', 'enumMember'],
+                    format: ['camelCase', 'PascalCase', 'UPPER_CASE', 'snake_case'],
+                    leadingUnderscore: 'allow',
+                    trailingUnderscore: 'allow'
+                }
+            ],
+
+            // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/consistent-destructuring.md
+            'unicorn/better-regex': 'warn',
+
+            // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/better-regex.md
+            'unicorn/consistent-destructuring': 'warn',
+
+            // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/filename-case.md
+            'unicorn/filename-case': [
+                'error',
+                {
+                    case: 'camelCase'
+                }
+            ],
+
+            // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/catch-error-name.md
+            'unicorn/catch-error-name': [
+                'error',
+                {
+                    name: 'error'
+                }
+            ],
+
+            // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/string-content.md
+            // 'unicorn/string-content': [
+            //     'error',
+            //     {
+            //         'patterns': {
+            //             'unicorn': '🦄',
+            //             'awesome': {
+            //                 'suggest': '😎',
+            //                 'message': 'Please use `😎` instead of `awesome`.'
+            //             },
+            //             'cool': {
+            //                 'suggest': '😎',
+            //                 'fix': false
+            //             }
+            //         }
+            //     }
+            // ],
+
+            'unicorn/no-anonymous-default-export': 'off'
         }
-      ],
+    },
 
-      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/consistent-destructuring.md
-      'unicorn/better-regex': 'warn',
-
-      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/better-regex.md
-      'unicorn/consistent-destructuring': 'warn',
-
-      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/filename-case.md
-      'unicorn/filename-case': [
-        'error',
-        {
-          'case': 'camelCase'
+    /**
+     *
+     */
+    {
+        files: ['tests/**/*', '*.spec.ts', '*.test.ts', '*.d.ts'],
+        rules: {
+            'unicorn/filename-case': 'off'
         }
-      ],
+    },
 
-      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/catch-error-name.md
-      'unicorn/catch-error-name': [
-        'error',
-        {
-          'name': 'error'
+    {
+        files: ['tests/**/*', '*.spec.ts', '*.test.ts'],
+        extends: [
+            tseslint.configs.recommended,
+        ],
+        languageOptions: {
+            globals: {
+                ...globals.jest,
+                ...globals.browser
+            }
         }
-      ],
-
-      // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/HEAD/docs/rules/string-content.md
-      // 'unicorn/string-content': [
-      //     'error',
-      //     {
-      //         'patterns': {
-      //             'unicorn': '🦄',
-      //             'awesome': {
-      //                 'suggest': '😎',
-      //                 'message': 'Please use `😎` instead of `awesome`.'
-      //             },
-      //             'cool': {
-      //                 'suggest': '😎',
-      //                 'fix': false
-      //             }
-      //         }
-      //     }
-      // ],
-
-      'unicorn/no-anonymous-default-export': 'off',
     }
-  },
-
-  /**
-   *
-   */
-  {
-    files: ['tests/**/*', '*.spec.ts', '*.test.ts', '*.d.ts'],
-    rules: {
-      'unicorn/filename-case': 'off'
-    }
-  },
-
-
-
-  {
-    files: ['tests/**/*', '*.spec.ts', '*.test.ts'],
-
-    languageOptions: {
-      globals: {
-        ...globals.jest
-      }
-    }
-  }
 )
